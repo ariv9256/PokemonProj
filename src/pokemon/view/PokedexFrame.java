8bfd5686a414532e41bfa9d870e62be2f5740820
Angela Rivas <snowcats001@gmail.com>
2019-02-04 13:56:10 -0700

    Added physical frame pieces and added all setupValues in panel

--- src/pokemon/view/PokedexFrame.java
+++ src/pokemon/view/PokedexFrame.java
@@ -11,10 +11,18 @@ public class PokedexFrame extends JFrame
 	public PokedexFrame()
 	{
 		super();
+		this.pokedexController = pokedexController;
+		this.pokedexPanel = new PokedexPanel(pokedexController);
+		
+		setupFrame();
 	}
 	private void setupFrame()
 	{
-		
+		this.setContentPane(pokedexPanel);
+		this.setSize(800, 800);
+		this.setTitle("GOTTA CATCH 'EM ALL");
+		this.setResizable(true);
+		this.setVisible(true);
 	}
 	
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -10,5 +10,28 @@ public class PokedexPanel extends JPanel
 	private PokedexController pokedexController;
 	private PokedexPanel pokedexPanel;
 	private JLabel pursuit;
+	private SpringLayout appLayout;
+	
+	public PokedexPanel(PokedexController pokedexController)
+	{
+		super();
+		this.pokedexController = pokedexController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void setupPanel()
+	{
+		
+	}
+	private void setupLayout()
+	{
+		this.setLayout(appLayout);
+	}
+	private void setupListeners()
+	{
+		
+	}
 	
 }
